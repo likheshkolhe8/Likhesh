fded1aa180b14a9a67cf9578f72db5a651bad1be
likheshkolhe8 <37599720+likheshkolhe8@users.noreply.github.com>
2020-04-24 23:39:17 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
-function hi{
+function hi
+{
 abc=$(ls -l | grep "^-"| wc -l)
 while 1
-do
-echo "enter your guess"
-read answer
-id [$answer -lt $abc]
-then
-echo "less"
-elif [$answer -gt $abc]
-then
-echo "greater"
-else
-echo "answer is correct"
-break;
-fi
+  do
+    echo "enter your guess"
+    read answer
+    id [$answer -lt $abc]
+    then
+    echo "less"
+    elif [$answer -gt $abc]
+    then
+    echo "greater"
+    else
+    echo "answer is correct"
+    break;
+    fi
 done
 }
 echo "recent directory
